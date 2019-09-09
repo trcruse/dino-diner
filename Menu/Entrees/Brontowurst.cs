405155816ccb70a2325670633fb54838085f9dbd
trcruse <trcruse@ksu.edu>
2019-09-09 17:19:56 -0500

    save

---
+++ Menu/Entrees/Brontowurst.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    class Brontowurst
+    {
+        private bool bun = true;
+        private bool peppers = true;
+        private bool onions = true;
+        
+        public double Price { get; set; }
+
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "brautwurst"};
+                if (bun) ingredients.Add("Whole-Wheat Bun");
+                if (peppers) ingredients.Add("Peppers");
+                if (onions) ingredients.Add("Onions");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+
+        public void HoldOnions()
+        {
+            this.onions = false;
+        }
+
+
+    }
+}
---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    class DinoNuggets
+    {
+        private bool chickenNug = true;
+
+        public double Price { get; set; }
+
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() {};
+                if (chickenNug) ingredients.Add("Chicken Nuggets");
+                
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59;
+        }
+
+
+    }
+}
