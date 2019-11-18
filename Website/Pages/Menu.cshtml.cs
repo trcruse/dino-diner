115cf238cafcde6450a83eb485ab162a7c02c69e
Taylor Cruse <trcruse@ksu.edu>
2019-11-17 21:56:01 -0600

    Website Milestone 1

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+
+        /// <summary>
+        /// Instance calling Menu
+        /// </summary>
+        public Menu Menu { get; }
+
+        public void OnGet()
+        {
+          
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
